5a348e2be844d2c42a6dfda9a1b81f15564e6ab9
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-11-05 19:17:46 +0530

    ✨ feat: introduction to Redux store

--- src/store.js
+++ src/store.js
@@ -1,39 +1,54 @@
-/** INTRODUCTION TO REDUX
- * here we will be learning redux in isolation without React
- * NOTE: Redux is similiar to useReducer hook, So if you are familiar with Reducer you can understand Redux
+import { createStore } from 'redux';
+
+/** INSTALLING REDUX IN PROJECT
+ * So previously we did was writing an object and a function
+ * So we didn't used Redux yet, now after setting up everything now we need it
  *
- * 1) Create initialState
+ * 1) To install redux, in terminal type:
+ * * npm i redux
  *
- * 2) Define reducer function
- * i) it receives state & action
- * ii) goal of this reducer is to calculate the new state based on the current state and on the received action
- * iii) it's also important to remember that reducer's are not allowed to modify the existing state an they are
- * also not allowed to do any asynchronous logic or other side effects, instead what we should do with reducers is
- * to place as much logic as possible inside of them.
- * iv) one thing is different in this redux's reducer and useReducer hook's reducer is that we directly pass
- * the initial state as the default state
- * ? useReducer's reducer :
- * * function reducer(state, action) {}
- * ? Redux's reducer:
- * * function reducer(state = initialState, action) {}
- * v) using the switch statement in the reducer function to select action type.
- * so the actions that are going to be dispatched to this Redux store
- * they will again have to shape of type and a payload like we do in useRecuder's function
- * that's why it is now easy to learn redux
+ * 2) Now out of this redux package we will now take:
+ * * create store method
+ * i) so let's import it on top from redux
+ * ii) as soon as we import it we can see that it has a line-through on it
+ * the reason for that is the Redux Team declared that this method is depracated.
+ * As i mentioned earlier there is now more modern way of writing Redux which is Redux tool-kit.
+ * But it is way better to first learn Redux in this way and once we have all the Redux knowledge
+ * then we can transition to the Redux tool kit, otherwise everything will really just seem like magic
+ * as it just works too easily and you will have no idea what's just going on...
  *
- * 3) Now let's come to the cases we provide in this switch case
- * i) it is advised to write these actions names, these actions names should model what happens
- * or what should happen
- * ii) so we write them in a shape of state domain eg: account/deposit so it's domain/event-name
+ * 3) Calling createStore
+ * i) So now let's call the createStore function with the reducer and store it inside store
+ * * const store = createStore(reducer)
+ * and that's it
+ * ii) now we can dispatch actions on the store we created
+ * eg, we can do:
+ * * store.dispatch()
+ * This is going to work in the exact same way as the dispatch function that we got as a result
+ * of calling the useReducer hook, so this dispatch function is basically what we already know
+ * from useReducer and so this is where now we can pass in an event
+ * * store.dispatch({type: 'account/deposit', payload: 500});
+ * iii) now how do we execute this and see the result
+ * a) go to index.js file, and import entire store file there
+ * b) import './store'
+ * c) by doing this we will be able to run the store.js code
+ * d) so for example if we console.log anything in this store.js
+ * the output will be shown on log when we run this application
+ * e) so what we are interested in to log to the console is:
+ * * console.log(store.getState());
+ * This will show us the current state of our store
+ * as the result we get on the console:
+ * ! {balance: 500, loan: 0, loanPurpose: ''};
+ * iv) now we have our redux working in a simple way
  *
- * 4) We give a default value
- * i) but in useReducer's reducer function we used to throw an error in default val:
- *   default:
- *   throw new Error
- * ii) but in case of redux's reducer function for some reason it is adviced to not do that and instead
- * simply return the original state, so in case that the reducer receives an action that doesn't know
- * about it will simply return the original state back. So the state will simply not be updated, but there
- * also won't be an error
+ * 4) Now let's store account/requestLoan but in here in payload we will be passing
+ * an object which we haven't done something like this before before, but this is perfectly fine and nothing to worry
+ * as by doing this we can pass in multiple pieces of data.
+ * 
+ store.dispatch({ type: 'account/requestLoan', payload: { amount: 1000, purpose: ' Buy a car' },
+});
+console.log(store.getState());
+ *
  */
 
 const initialState = {
@@ -44,17 +59,22 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'action/deposit':
+    case 'account/deposit':
       return { ...state, balance: state.balance + action.payload };
 
-    case 'action/withdraw':
+    case 'account/withdraw':
       return { ...state, balance: state.balance - action.payload };
 
-    case 'action/requestLoan':
+    case 'account/requestLoan':
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
 
-    case 'action/payLoan':
+    case 'account/payLoan':
       return {
         ...state,
         loan: 0,
@@ -66,3 +86,17 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: 'account/deposit', payload: 500 });
+console.log(store.getState());
+
+store.dispatch({
+  type: 'account/requestLoan',
+  payload: { amount: 1000, purpose: ' Buy a car' },
+});
+console.log(store.getState());
+
+store.dispatch({ type: 'account/payLoan' });
+console.log(store.getState());
