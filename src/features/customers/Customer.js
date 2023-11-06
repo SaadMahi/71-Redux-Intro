c1d0c17f2eb79ed6f29a2d1469baaf0a40b4b7e6
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-11-06 13:53:31 +0530

    ✨ feat: reading data from redux store to app

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,3 +1,21 @@
+/** READING DATA FROM REDUX STORE
+ * In order to read data from Redux store all we have to do is use hook:
+ * * useSelector()
+ * which is provided by react-redux package which we instaleed previously
+ *
+ * 1) useSelector() hook
+ * i) this useSelector hook takes in a callBack function and this function takes
+ * as the single argument the entire redux store
+ * * useSelector(store => )
+ * ii) then from this store we can simply get the data we want eg:
+ * * useSelector(store => store.customer);
+ * this store.customer is exactly what we have writting in store.js file where we have
+ * rootReducer inside and inside of it we have customer
+ * iii) then after getting the data we can store that into any variable we want
+ * * const customer = useSelector(store => store.customer);
+ *
+ */
+
 function Customer() {
   return <h2>👋 Welcome, %NAME%</h2>;
 }
