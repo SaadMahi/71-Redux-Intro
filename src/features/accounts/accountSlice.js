a8b0358844994d080535ecc84f0221a4a9ec7358
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-11-06 19:38:57 +0530

    ✨ feat: how to use middleware and thunks

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -40,8 +40,61 @@ export default function accountReducer(state = initialStateAccount, action) {
 
 //*__________ ACTION CREATORS __________*//
 
-export function deposit(amount) {
-  return { type: 'account/deposit', payload: amount };
+/**  PLAYING WITH CURRENCY
+ * so in this deposit besides accepting amount we also need to accept an currency
+ * so as we dispatch amount we will now also pass in the currency, so let's add currency parameter besides amount
+ *
+ * So here we will be setting a condition, if currency set by the user is in USD
+ * then return the values as it is but if not we will now require to call the API
+ * for converting the currency into USD
+ *
+ * and to do that APi call we will need thunk
+ * so now in else condition
+ * we will return a function and when we return a function
+ * redux knows that this function is an asynchronous action
+ * that we want to execute before dispatching anything to the store
+ *
+ * So in order to later dispatch this function that redux will call internally
+ * will get access to the dispatch function and to the current state known as getState:
+ * and inside this function we will do our Api call.
+ * * return function(dispatch, getState){ // APi call}
+ * Once we are done doing everything in end we will return the action
+ *
+ * NOTE in the end we do not need to return but instead we dispatch() the converted value
+ *
+ * 2) So to sum up,
+ * i) first we dispatch in accounts operation:
+ * *  dispatch(deposit(depositAmount, currency));
+ * and in the case do not have currency in the USD dollars then this
+ * * deposit(depositAmount, currency) inside the dispatch
+ * will return a function, and that function is right below you can see the async function
+ *
+ * ii) and then redux will call this async function and because of that we can use
+ * the dispatch function in the parameter so we can later on call this function when
+ * we want to dispatch a action in the end to the store.
+ * And this all already works nicely
+ * ! figure 2.1 for output
+ *
+ * iii) So into observation this entire async function
+ * this isour middleWare and you can see that it really sits between the dispatching and the store
+ * a) so when we dispatch the action and the currency is different then the USD then that action
+ * actually never reaches the store because instead this async code will here be executed
+ * and only after this is finished we then dispatch new action to our store with the new value
+ * and in conclusion this async really fits in the middle, therefore the name is Middleware
+ */
+
+export function deposit(amount, currency) {
+  if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+
+  return async function (dispatch, getState) {
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    dispatch({ type: 'account/deposit', payload: converted });
+  };
 }
 export function withdraw(amount) {
   return { type: 'account/withdraw', payload: amount };
--- src/store.js
+++ src/store.js
@@ -1,6 +1,42 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
+import thunk from 'redux-thunk';
+
+/** MIDDLEWARE AND THUNKS
+ * Let's use redux thunks to implement a feature where the User
+ * can deposit the money into the account in a foreign currency
+ * which will then be converted calling an external Api
+ *
+ * ! refer figure 1.1 for the below explanation
+ * So the idea is when ever user deposits some money in deposit input
+ * they can select which currency that money is for example user selectes EURO
+ * and if the selected currency by the User is different than the USD then we
+ * to convert the user's input money back to USD before it get's deposited
+ * into the account means before that deposit action is dispatched to the store
+ *
+ * That's where the redux Thunk comes into the play
+ * So we will have this MiddleWare sitting in between dispatching the action
+ * as we click on the deposit button and the action reaching the store
+ *
+ * In order to use this MiddleWare we need to follow these 3 steps:
+ * 1) install the MiddleWare package
+ * * npm i redux-thunk
+ * a) import thunk function from redux thunk at top
+ * * import thunk from 'redux-thunk';
+ *
+ * 2) Apply the middleware to our redux store we created
+ * a) pass in another argument in create store function, here we need to call apply middleware function,
+ * this is provided by redux and into this applyMiddleware function we pass in our middleware
+ * and that middleware is thunk that we imported from redux-thunk
+ * * const store = createStore(rootReducer, applyMiddleware(thunk));
+ * So by doing all above we told redux or we told our store, that we want to use thunk middleware
+ * in ou application and so let's now go to action creator in account slice
+ * which is responsible for depositing money into the account
+ *
+ *
+ * 3) We use the middleware in our action creator functions
+ */
 
 // !__________REDUCER COMBINER__________! \\
 const rootReducer = combineReducers({
@@ -9,6 +45,6 @@ const rootReducer = combineReducers({
 });
 
 // !__________CREATED STORE__________! \\
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
