0440080be08cb4efb31f4a310555f26e57f9c325
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-11-06 11:38:12 +0530

    ✅ feat: created slice files, ready to move content

--- src/store.js
+++ src/store.js
@@ -1,32 +1,20 @@
 import { combineReducers, createStore } from 'redux';
 
-/** USING Account reducer & Customer reducer COMBINED
- * i) we already have this account reducer stored in store
- * * const store = createStore(accountReducer);
- * ii) but how do we now store this Customer Reducer.
- * So do you remember that in Redux we don't dispatch actions
- * directly to the reducer but to the store and that's what we did using
- * * store.dispatch(deposit(500));
- * iii) now we cannot simply pass in another customerReducer to this store:
- * * const store = createStore(accountReducer);
- * instead we need to combine all the reducers we have in
- * order to create one so called root reducer, because this reducer
- * * createStore();
- * receives is always considered root reducer
- * iv) so we combine all the reducers, we will use
- * * combineReducers(); // function from redux (IMPORT AT TOP)
- * in this we will specify an object and then we need to give
- * each reducers a meaning full name
- * * const rootReducer = combineReducers({ account: accountReducer, customer: customerReducer })
- * v) now in end we use this rootReducer in store
- * * const store = createStore(rootReducer);
- * vi) now we can dispatch easily just by using function name
- * store.dispatch(createCustomer('Mahimkar Saad', '2105690238')); // console.log(store.getState());
- * store.dispatch(deposit(500));  // console.log(store.getState());
- * and we will get the output on our console do check the figure 1.1
- * vii) and point to be noted here, redux is very smart enough to know if the action type belongs to
- * to the customerReducer or accountReducer, as you can see in point vi) we dispatched
- * createCustomer and deposit from both reducers using same convention 'store.dispatch'
+/** PROFESSIONAL REDUX FILE STRUCTURING USING (STATE SLICES)
+ * so when we have multiple initial states, reducers & action creators our got starts to get out of our hands
+ * so as we have two different domains Account and the Customer both are here being used in 1 single file
+ * so imagine we had 10 Reducer's like these instead of two, which is not practical at all
+ *
+ * so we will be working with two features, accountReducer and cutomerReducer
+ *
+ * i) create folder in src: features
+ * ii) in features create folder's for each features: accounts & customers
+ * iii) move your ready made files to their respective folder's and update their imports
+ * iv) once done with above create redux files in each folder: accountSlice.js & customerSlice.js.
+ * So slice is basically a piece or just a part of total state so the entire state lives in the store
+ * So in slice file we basically take one slice of that state
+ * v) now the idea is to in each slice file we co-locate as much as the redux logic as possible in one file
+ * vi) So we will be placing the reducer, initial state, action creators
  */
 
 //**************** ACCOUNT REDUCER ****************//
