9841ea8b91f90553448aae86fffebe2677cef3aa
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-11-06 20:48:39 +0530

    ✨ feat: how to use Redux developer tools

--- src/store.js
+++ src/store.js
@@ -2,40 +2,47 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-/** MIDDLEWARE AND THUNKS
- * Let's use redux thunks to implement a feature where the User
- * can deposit the money into the account in a foreign currency
- * which will then be converted calling an external Api
+/** REDUX DEVELOPER TOOLS
+ * Redux comes with some amazing developer tools
+ * just like React, let's now install and use those developer tools
+ * installing the redux dev tool is a 3 step process:
+ * 1) so go install the Redux dev tool extensio into your browser
+ * 2) install the corresponding npm package
+ * * npm i redux-devtools-extension
+ * i) now this npm package gives us one function that we need to use we need to import that at top
+ * * import { composeWithDevTools } from 'redux-devtools-extension';
+ * ii) now we need to wrap the Middleware with the composeWithDevTools
+ * iii) give it a save and we are good to go, now it really doesn't matters why we need to write
+ * it in this way or what's going behind the scenes, but that's it we need to do
+ * iv) but by now we should get the new tab for redux in our developer tools:
+ * ! refer figure 1
  *
- * ! refer figure 1.1 for the below explanation
- * So the idea is when ever user deposits some money in deposit input
- * they can select which currency that money is for example user selectes EURO
- * and if the selected currency by the User is different than the USD then we
- * to convert the user's input money back to USD before it get's deposited
- * into the account means before that deposit action is dispatched to the store
+ * v) when we click this we get our Redux dev tools:
+ * ! refer figure 2
  *
- * That's where the redux Thunk comes into the play
- * So we will have this MiddleWare sitting in between dispatching the action
- * as we click on the deposit button and the action reaching the store
+ * 3) Now to test these we need to dispatch some actions
+ * ! refer figure 3, 3.1 //
+ * circled red is our first action dispatched with the name customer/createCustomer
  *
- * In order to use this MiddleWare we need to follow these 3 steps:
- * 1) install the MiddleWare package
- * * npm i redux-thunk
- * a) import thunk function from redux thunk at top
- * * import thunk from 'redux-thunk';
+ * 4) we can also see the action object, with the type and the payload
+ * ! figure 4
+ * and also see the state that resulted after dispatching the action
+ * ! figure 4.1
+ * i) cool feature in it we also have is that we can jump back to previous state
+ * ii) for example we have balance 0, then we deposited money now we have balance
+ * 200, now we can click jump on previous state which will give us zero balance on UI
+ * iii) you can also use slider(video player) to track each and every state change
+ * iv) So this can be really use full when we have alot of different state transitions
+ * and a really complex state, then we can time travel between these state transitions
+ * in order to maybe find some bugs or some problems we might encounter
  *
- * 2) Apply the middleware to our redux store we created
- * a) pass in another argument in create store function, here we need to call apply middleware function,
- * this is provided by redux and into this applyMiddleware function we pass in our middleware
- * and that middleware is thunk that we imported from redux-thunk
- * * const store = createStore(rootReducer, applyMiddleware(thunk));
- * So by doing all above we told redux or we told our store, that we want to use thunk middleware
- * in ou application and so let's now go to action creator in account slice
- * which is responsible for depositing money into the account
+ * v) you can also dispatch manually as per your will
+ * ! figure 4.2, 4.3
+ * so it updated the name right there, so this can be quite helpful if we just want to
+ * manually dispatch some actions without having to wireup like some buttons in the Ui
  *
- *
- * 3) We use the middleware in our action creator functions
  */
 
 // !__________REDUCER COMBINER__________! \\
@@ -45,6 +52,9 @@ const rootReducer = combineReducers({
 });
 
 // !__________CREATED STORE__________! \\
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
